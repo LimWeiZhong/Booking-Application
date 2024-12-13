eab02cc7b008f12aa5568a4622cb9440b536d570
LWZ <117212909+LWZ1992xai@users.noreply.github.com>
2024-12-13 15:37:18 +0800

    Update Bookingapp.py

--- Bookingapp.py
+++ Bookingapp.py
@@ -65,7 +65,7 @@ def log_transaction(action, room, date, start_time, end_time, user):
 image_path = "images/background.jpg"
 st.image(image_path, use_column_width=True)
 st.set_page_config(
-    initial_sidebar_state = "collapsed"  # Collapse the sidebar
+    initial_sidebar_state="collapsed"  # Collapse the sidebar
 )
 tabs = st.tabs(["Book a Room", "Edit or Cancel Booking"])
 
